602d6f7ea2a9f8a097044c6936c7acd835f98b0c
Robert <robertale45@gmail.com>
2024-05-07 21:49:30 -0300

    FINALIZANDO PROJETO CENTRO CIRURGICO

--- projetos/HRT/form-paciente/script.js
+++ projetos/HRT/form-paciente/script.js
@@ -1,6 +1,4 @@
-document.getElementById('bt-apagar').addEventListener('click', apagar);
-document.getElementById('bt-gravar').addEventListener('click', gravar);
-document.getElementById('bt-novo').addEventListener('click', limparForm);
+
 let lsPessoa = [];
 
 let tpStatus = {
