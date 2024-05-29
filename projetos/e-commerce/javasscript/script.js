bd55d7cb0898bcea858d4dea6c737c2fe4076539
mellodev11 <robertale45@gmail.com>
2024-05-29 16:26:14 -0300

    fazendo mudanças no js

--- projetos/e-commerce/javasscript/script.js
+++ projetos/e-commerce/javasscript/script.js
@@ -6,9 +6,9 @@ for (const p of produtos) {
     <img src="${p.img}" alt="${p.dsImg}">
     <div class="info">
         <h3>${p.nome}</h3>
-        <h4>6 fatias<span>R$${p.fatias6}</span></h4>
-        <h4>8 fatias<span>R$${p.fatias8}</span></h4>
-        <h4>12 fatias<span>R$${p.fatias12}</span></h4>
+        <h4><span>${p.descricao}</span></h4>
+        <h4>Valor :<span>R$${p.valor}</span></h4>
+        
         <button id="id${id}" class="pedir">pedir agora</button>
     </div>
 </div>`
