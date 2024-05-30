344b689eca75f4e1cf1c4f107be8b07881d076f6
robert <robertale45@gmail.com>
2024-05-29 21:16:04 -0300

    finalizando footer

--- projetos/e-commerce/javasscript/produtos.js
+++ projetos/e-commerce/javasscript/produtos.js
@@ -9,59 +9,59 @@ const produtos = [
     },
     {
 
-        img: "img/minoxidil.png",
-        nome: "Minoxidil Kirkiland",
-        descricao: "5% Importado",
-        valor: 85.00
+        img: "img/NAVIFORCE.JPEG",
+        nome: "Naviforce",
+        descricao: "Dourado",
+        valor: 150.00
     },
     {
 
-        img: "img/minoxidil.png",
-        nome: "Minoxidil Kirkiland",
-        descricao: "5% Importado",
-        valor: 85.00
+        img: "img/amazfit.jpeg",
+        nome: "Amazfit",
+        descricao: "Gts 4 Mini",
+        valor: 750.00
     },
     {
 
-        img: "img/minoxidil.png",
-        nome: "Minoxidil Kirkiland",
-        descricao: "5% Importado",
-        valor: 85.00
+        img: "img/Anker.jpeg",
+        nome: "Fone ANKER",
+        descricao: "Anker Life",
+        valor: 1000.00
     },
     {
 
-        img: "img/minoxidil.png",
-        nome: "Minoxidil Kirkiland",
-        descricao: "5% Importado",
-        valor: 85.00
+        img: "img/garrafa stanley2.jpeg",
+        nome: "Garrafa Stanley",
+        descricao: "GARRAFA STANLEY",
+        valor: 320.00
     },
     {
 
-        img: "img/minoxidil.png",
-        nome: "Minoxidil Kirkiland",
-        descricao: "5% Importado",
-        valor: 85.00
+        img: "img/fone baseus.JPEG",
+        nome: "Fone Baseus",
+        descricao: "Baseus fone",
+        valor: 350.00
     },
     {
 
-        img: "img/minoxidil.png",
-        nome: "Minoxidil Kirkiland",
-        descricao: "5% Importado",
-        valor: 85.00
+        img: "img/applewatch.JPEG",
+        nome: "Smartwatch",
+        descricao: "APPLEWATCH",
+        valor: 8499.00
     },
     {
 
-        img: "img/minoxidil.png",
-        nome: "Minoxidil Kirkiland",
-        descricao: "5% Importado",
-        valor: 85.00
+        img: "img/iphone 15 rosa.jpeg",
+        nome: "IPHONE",
+        descricao: "IPHONE15",
+        valor: 7000.00
     },
     {
 
-        img: "img/minoxidil.png",
-        nome: "Minoxidil Kirkiland",
-        descricao: "5% Importado",
-        valor: 85.00
+        img: "img/flamengo camisa 1.jpg",
+        nome: "Camisa importada",
+        descricao: "Camisas Tailandesas",
+        valor: 140.00
     }
 
 
--- projetos/e-commerce/javasscript/script.js
+++ projetos/e-commerce/javasscript/script.js
@@ -1,16 +1,12 @@
 let itensCatalogo = document.querySelector(".itens-catalogo");
 let id = 0;
 for (const p of produtos) {
-    itensCatalogo.innerHTML += `            
-    <div class="">
-    <img src="${p.img}" alt="${p.dsImg}">
-    <div class="info">
-        <h3>${p.nome}</h3>
-        <h4><span>${p.descricao}</span></h4>
-        <h4>Valor :<span>R$${p.valor}</span></h4>
-        
-        <button id="id${id}" class="pedir">pedir agora</button>
-    </div>
-</div>`
+    itensCatalogo.innerHTML += `   
+        <div class="info">
+          <img src="${p.img}" alt="${p.dsImg}" />
+          <h3>${p.nome}</h3>
+          <h5>${p.descricao}:<span>R$${p.valor}</span></h5>
+          <button id="id${id}" class="pedir">pedir agora</button>
+        </div>`
 id++;
 }
