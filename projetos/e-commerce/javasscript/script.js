d75e01eb3d15d58847ae27cb44b9c0823f8aa96b
mellodev11 <robertale45@gmail.com>
2024-06-13 13:58:31 -0300

    BOTAO REMOVER E ADICIONAR E ADD AO CARRINHO

--- projetos/e-commerce/javasscript/script.js
+++ projetos/e-commerce/javasscript/script.js
@@ -1,5 +1,6 @@
-// const menuContent = document.querySelector('.content');
-// const show = true;
+let menuContent = document.querySelector('.content');
+let menuToggle = menuContent.querySelector('.menu-toggle');
+let show = true;
 
 
 
@@ -30,6 +31,78 @@ continuar.addEventListener('click', ()=>{
     telaCarrinho.classList.toggle('ocultar-tela-carrinho');
 })
 
+//até aqui tudo OK 
+
+let lsPedido = document.querySelectorAll('.pedir');
+for (const bt of lsPedido) {
+    bt.addEventListener('click', () => {
+        let id = bt.id.replace('id', '');
+        let car = document.getElementById('car')
+        bt.classList.toggle('selecionado');
+        if (bt.innerHTML == 'REMOVER') {
+            produtos[id].quantidade = 0;
+            bt.innerHTML = 'pedir agora'
+            car.innerHTML = ` <i class='bx bx-cart-add'></i>`
+        } else {
+            produtos[id].quantidade = 1;
+            bt.innerHTML = 'REMOVER';
+            car.innerHTML = ` <i class='bx bx-cart-add'></i>`
+        }
+        atualizarTabela();
+    });
+}
+
+//ATÉ AQUI REMOVER E ADC OK 
+
+
+
+let tbody = document.querySelector('tbody');
+function atualizarTabela() {
+    tbody.innerHTML = '';
+    let total = 0;
+    let id = 0;
+    for (const p of produtos) {
+        if (p.quantidade > 0) {
+            tbody.innerHTML += `
+              <tr>
+                            <td><img src="${p.img}"> </td>
+                            <td><span class="valor">${p.nome} =R$ ${p.valor},00 </span></td>
+                            <td> ${p.quantidade} </td>
+                            <td> R$ ${p.quantidade * p.valor},00 </td>
+                            <td>
+                                <i class="bi bi-plus-square-fill" id="plus${id}"></i>
+                    
+                                <i class="bi bi-dash-square-fill" id="dash${id}"></i>
+                            </td>
+                        </tr>
+            
+           `;
+            total += p.quantidade * p.valor;
+        }
+        id++;
+    }
+    document.querySelector('#total-pedido').innerHTML = `Valor total do pedido = R$ ${total},00`;
+    atualizarPlusDash('plus');
+    atualizarPlusDash('dash');
+}
+function atualizarPlusDash(tipo) {
+    let botoes = document.querySelectorAll(`.bi-${tipo}-square-fill`);
+    for (const bt of botoes) {
+        bt.addEventListener('click', () => {
+            let id = bt.id.replace(tipo, '');
+            if (tipo == 'plus') {
+                produtos[id].quantidade++;
+            }
+            if (tipo == 'dash') {
+                produtos[id].quantidade--;
+                if (produtos[id].quantidade < 1) {
+                    document.getElementById('id' + id).click();
+                }
+            }
+            atualizarTabela();
+        });
+    }
+}
 
 
 
